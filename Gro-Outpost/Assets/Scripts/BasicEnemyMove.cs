63875e1878a334b04f8aea97982c1029dbb6fbde
Twilit <37117133+Twilit@users.noreply.github.com>
2019-09-24 00:20:04 +0100

    player movement and various assets added

---
+++ Gro-Outpost/Assets/Scripts/BasicEnemyMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyMove : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.transform.position);
+    }
+}
---
+++ Gro-Outpost/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    float runSpeed = 4f;
+
+    float x = 0f;
+    float y = 0f;
+    Vector3 dir;
+
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        dir = new Vector3(x, y, 0);
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.transform.position + dir.normalized * runSpeed * Time.fixedDeltaTime);
+    }
+}
