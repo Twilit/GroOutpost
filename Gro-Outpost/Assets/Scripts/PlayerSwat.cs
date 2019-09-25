6b839bf224a97055e18a5e9f53c0c8f70223a2de
Twilit <37117133+Twilit@users.noreply.github.com>
2019-09-25 01:16:18 +0100

    Added basic flyswatter

---
+++ Gro-Outpost/Assets/Scripts/PlayerSwat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwat : MonoBehaviour
+{
+    public Transform swatter;
+    Animator swatAnim;
+
+    bool swatting = false;
+
+    void Start()
+    {
+        swatAnim = swatter.GetChild(0).GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !swatting)
+        {
+            SwingSwatter();
+        }
+    }
+
+    void SwingSwatter()
+    {
+        swatting = true;
+        swatAnim.SetTrigger("startSwat");
+    }
+
+    public void EndSwing()
+    {
+        swatting = false;
+
+    }
+}
