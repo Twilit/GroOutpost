fc607be276fc4a606da242c4ebba81b307814aad
RyanBCFC2000 <45456015+RyanBCFC2000@users.noreply.github.com>
2019-09-26 15:30:33 +0100

    Level select
    complete level selection

---
+++ Gro-Outpost/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+        public void GotoMainScene()
+        {
+            SceneManager.LoadScene("Stage1");
+        }
+    public void GotoLevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+}
