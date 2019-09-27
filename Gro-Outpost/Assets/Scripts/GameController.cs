55f2649d91a7bd618bc37b013fe59a9d44971be0
Twilit <37117133+Twilit@users.noreply.github.com>
2019-09-27 01:03:59 +0100

    gamecontroller in progress

--- Gro-Outpost/Assets/Scripts/GameController.cs
+++ Gro-Outpost/Assets/Scripts/GameController.cs
@@ -6,10 +6,16 @@ public class GameController : MonoBehaviour
 {
     public enum PlantPhases { Seed, Seedling, Bulb, Flower };
 
+    public PlantPot[] pots;
+    public Sprite[] plantPhaseSprites;
 
     void Start()
     {
-        
+        for (int i = 0; i < pots.Length; i++)
+        {
+            pots[i].ChangeSprite(plantPhaseSprites[0]);
+            pots[i].ChangePhase(PlantPhases.Seed);
+        }
     }
 
     void Update()
---
+++ Gro-Outpost/Assets/Scripts/PlantPot.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlantPot : MonoBehaviour
+{
+    GameController.PlantPhases currentPhase;
+    SpriteRenderer plantSprite;
+
+    float growthTimer = 0f;
+    bool needWater = false;
+
+    public Text waterText;
+
+    void Start()
+    {
+        plantSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+
+        growthTimer = 0f;
+    }
+
+    void Update()
+    {
+        NeedWaterTimer();
+    }
+
+    public void ChangeSprite(Sprite newSprite)
+    {
+        plantSprite.sprite = newSprite;
+    }
+    public void ChangePhase(GameController.PlantPhases newPhase)
+    {
+        currentPhase = newPhase;
+    }
+
+    void NeedWaterTimer()
+    {
+        if (growthTimer < 30f)
+        {
+            growthTimer += Time.deltaTime;
+        }
+        else
+        {
+
+        }
+    }
+
+    IEnumerator NeedWaterIndicator()
+    {
+        while(true)
+        {
+            waterText.text = "";
+            yield return new WaitForSeconds(0.5f);
+            waterText.text = "WATER!";
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
