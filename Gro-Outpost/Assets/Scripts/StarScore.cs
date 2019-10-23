cfdf11aae14f68f9808bf6853443ce46e7ddc088
Twilit <37117133+Twilit@users.noreply.github.com>
2019-10-23 02:23:52 +0100

    start on scores

---
+++ Gro-Outpost/Assets/Scripts/StarScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarScore : MonoBehaviour
+{
+    public string currentLevelHS;
+    int highScore;
+    int currentScore;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey(currentLevelHS))
+        {
+            highScore = PlayerPrefs.GetInt(currentLevelHS);
+        }
+    }
+
+    void SaveScore()
+    {
+        if (currentScore > highScore)
+        {
+            PlayerPrefs.SetInt(currentLevelHS, currentScore);
+
+            PlayerPrefs.Save();
+        }
+    }
+
+    void DisplayScore()
+    {
+
+    }
+}
